0821db88ae5c86ebafe6024b131f9e71ced5ed0a
malimaaa-hue <milenaalvesdelimaaa@gmail.com>
2025-11-12 21:15:17 -0300

    parte do roteiro 3

--- apps/categories/views.py
+++ apps/categories/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import Category
+from .serializer import CategorySerializer
+
+
+# Após o comentario "# Create your views here." e crie as views "Category".
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer  
+    
