8ff024095906dc241401ff9e23e47f8da5173b93
malimaaa-hue <milenaalvesdelimaaa@gmail.com>
2025-11-16 18:57:04 -0300

    corrigindo views

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,17 +1,11 @@
-from django.shortcuts import render
 from .models import Product
 from rest_framework import viewsets
 from .serializer import ProductSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
 
-filter_backends = [DjangoFilterBackend]
-filterset_fields = ['name', 'description', 'category'] # Neste campo coloque somente.
-
-
-class ProductsViewSet(viewsets.ModelViewSet):
+class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
-    serializer_class = ProductSerializer 
+    serializer_class = ProductSerializer  
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['name', 'description', 'category']
