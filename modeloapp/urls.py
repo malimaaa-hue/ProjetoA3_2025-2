86cdebd638bc2a0acb8f9aa7c8e8bc4afa1cedeb
malimaaa-hue <milenaalvesdelimaaa@gmail.com>
2025-11-16 14:09:05 -0300

    urls na main

--- modeloapp/urls.py
+++ modeloapp/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 #from django.urls import path
 from django.urls import path, include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('categorias/', include('categories.urls', namespace='categories')),
-    
+    path('produtos/', include('products.urls', namespace='products')),
+
 ]
