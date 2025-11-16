a0ff732b6e7d5459d1564704e668ae485ca47e14
malimaaa-hue <milenaalvesdelimaaa@gmail.com>
2025-11-16 14:27:26 -0300

    teste

--- apps/products/views.py
+++ apps/products/views.py
@@ -3,7 +3,7 @@ from .models import Product
 from rest_framework import viewsets
 from .serializer import ProductSerializer
 
-# Create your views here.
+#Create your views
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer  
